4cc8071715dd5c2094e0b1feb660b338317920d9
Kun-Tai Lin <ktcyrusjl889@gmail.com>
2021-09-30 23:39:02 -0700

    Add new file

---
+++ c_to_f.py
@@ -0,0 +1,6 @@
+# Convertion from Celsius to Fahrenheit 
+# F = C*(9/5) + 32
+c = input('Please enter a Celsius temperature: ')
+c = float(c)
+f = c*9/5 + 32 
+print('The Fahrenheit temperature: ', f)
